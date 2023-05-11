757cae08d1ed0f9e75000931a3e7021eb98ad1af
endigma <endigma@mailcat.ca>
2023-05-11 13:42:12 -0300

    format & use default layer

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 
 describe("radix preset", () => {
-	it("should work", () => {
-		expect(1).toBe(1);
-	});
+  it("should work", () => {
+    expect(1).toBe(1);
+  });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -5,36 +5,36 @@ import unocss from "unocss/vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig(({ mode }) => ({
-	build: {
-		lib:
-			mode === "demo"
-				? false
-				: {
-						entry: "./src/index.ts",
-						name: "unocss-preset-radix",
-						fileName: "index",
-						formats: ["es", "cjs"],
-				  },
-		rollupOptions: {
-			external: ["unocss"],
-		},
-	},
-	plugins: [
-		dts({
-			insertTypesEntry: true,
-		}),
-		unocss({
-			presets: [
-				presetUno(),
-				presetRadix({
-					palette: radixColors,
-				}),
-			],
-			safelist: [
-				...radixColors.map((p) => `hue-${p}`),
-				...Array.from({ length: 12 }, (_, i) => `bg-hue${i + 1}`),
-				...Array.from({ length: 12 }, (_, i) => `bg-hue${i + 1}A`),
-			],
-		}),
-	],
+  build: {
+    lib:
+      mode === "demo"
+        ? false
+        : {
+            entry: "./src/index.ts",
+            name: "unocss-preset-radix",
+            fileName: "index",
+            formats: ["es", "cjs"],
+          },
+    rollupOptions: {
+      external: ["unocss"],
+    },
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+    unocss({
+      presets: [
+        presetUno(),
+        presetRadix({
+          palette: radixColors,
+        }),
+      ],
+      safelist: [
+        ...radixColors.map((p) => `hue-${p}`),
+        ...Array.from({ length: 12 }, (_, i) => `bg-hue${i + 1}`),
+        ...Array.from({ length: 12 }, (_, i) => `bg-hue${i + 1}A`),
+      ],
+    }),
+  ],
 }));
