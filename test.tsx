97d744010132ffa7fbdf5a7037800cd07b22d947
hmd <->
2024-10-12 12:04:06 +0330

    feat: test stuff

---
+++ test.tsx
@@ -0,0 +1,5 @@
+function Comp(){
+
+return (
+<button className="p-4 alias-danger-red bg-danger4 bg-my-color">Delete</button>
+)};
--- uno.config.ts
+++ uno.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
       // useP3Colors: true,
       // onlyOneTheme: 'dark',
       // safelistAliases: ['warning', 'danger4A'],
-      safelist: ['pink3A', 'white11A', 'blue' , 'warning', 'danger4A'],
+      // safelist: ['pink3A', 'white11A', 'blue' , 'warning', 'danger4A'],
     }),
   ],
 });
